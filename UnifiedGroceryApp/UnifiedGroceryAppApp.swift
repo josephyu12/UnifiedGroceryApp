af93dd2dfc47c3531d9b11e12c03e1927b2d61e9
josephyu12 <jyu.joeyu@gmail.com>
2023-01-27 09:31:00 -0500

    Initial Commit

---
+++ UnifiedGroceryApp/UnifiedGroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnifiedGroceryAppApp.swift
+//  UnifiedGroceryApp
+//
+//  Created by Joseph Yu on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct UnifiedGroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

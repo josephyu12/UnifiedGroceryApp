9c91ab9d7f70564d857471ee8fabb8f95d3ef0ce
travtran <ttran2@wpi.edu>
2023-03-20 14:46:18 -0400

    3/20/23

--- UnifiedGroceryApp/ContentView.swift
+++ UnifiedGroceryApp/ContentView.swift
@@ -9,57 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            
-            Image("background").ignoresSafeArea()
-            
-            VStack {
-                Spacer()
-                Image("logo")
-                Spacer()
-                HStack {
-                    Spacer()
-                    Image("card2")
-                    Spacer()
-                    Image("card3")
-                    Spacer()
+        TabView {
+            HomeView()
+                .tabItem() {
+                    Image(systemName: "custom.house")
+                    Text("Home")
                 }
-                Spacer()
-                
-                Image("dealbutton")
-                Spacer()
-                
-                HStack {
-                    Spacer()
-                    VStack {
-                        Text("Player")
-                            .font(.headline)
-                            .foregroundColor(Color.white)
-                            .padding(.bottom, 10.0)
-                        Text("0")
-                            .font(.largeTitle)
-                            .foregroundColor(Color.white)
-                        
-                    }
-                    Spacer()
-                    
-                    VStack {
-                        Text("CPU")
-                            .font(.headline)
-                            .foregroundColor(Color.white)
-                            .padding(.bottom, 10.0)
-                        Text("0")
-                            .font(.largeTitle)
-                            .foregroundColor(Color.white)
-                    }
-                    Spacer()
-                    
-                }
-                Spacer()
-            }
-            
         }
-
+        
     }
 }
 
---
+++ UnifiedGroceryApp/Views/RecipeView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  UnifiedGroceryApp
+//
+//  Created by Joseph Yu on 1/27/23.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        ZStack {
+            
+            Color("bluecolor").edgesIgnoringSafeArea(.all)
+            
+            /*VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                
+                Image("dealbutton")
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("asfasfsdafadsf")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    
+                }
+                Spacer()
+            }*/
+            
+        }
+
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeView()
+    }
+}

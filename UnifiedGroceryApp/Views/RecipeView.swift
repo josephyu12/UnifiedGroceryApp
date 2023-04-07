4ac489e4129628523211a56dba5f78e8f3093657
josephyu12 <jyu.joeyu@gmail.com>
2023-04-07 09:12:52 -0400

    fixed pr bug

--- UnifiedGroceryApp/Views/RecipeView.swift
+++ UnifiedGroceryApp/Views/RecipeView.swift
@@ -21,52 +21,55 @@ struct RecipeView: View {
     var body: some View {
         
         ZStack {
-          
-                  ZStack (alignment: .top) {
-            Color("redcolor").ignoresSafeArea()
             
-            Color("bluecolor").edgesIgnoringSafeArea(.top)
-            
-            ScrollView(showsIndicators: false) {
-                VStack {
-                    Color("bluecolor").edgesIgnoringSafeArea(.all)
-                    Text("Recipes").font(.largeTitle).padding(.top, 0.1)
-                    
-                    
-                    LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+            ZStack (alignment: .top) {
+                
+                Color("redcolor").ignoresSafeArea()
+                
+                Color("bluecolor").edgesIgnoringSafeArea(.top)
+                
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        Color("bluecolor").edgesIgnoringSafeArea(.all)
+                        Text("Recipes").font(.largeTitle).padding(.top, 0.1)
                         
-                        ForEach((0...9), id: \.self) {
-                            Button(action: {print("this is the action run")} ) {
-                                Image(systemName: "gear").foregroundColor(Color.black)
-                                    .padding(0).font(.system(size:24))
-                                Text("test123")
-                                    .font(.system(size: 30))
-                                    .foregroundColor(Color.black)
-                            }.font(.system(size: 30))
-                                .frame(minWidth: 0, maxWidth: UIScreen.screenWidth-10)
-                                .background(colors[$0 % colors.count])
-                                .cornerRadius(10)
+                        
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            
+                            ForEach((0...9), id: \.self) {
+                                Button(action: {print("this is the action run")} ) {
+                                    Image(systemName: "gear").foregroundColor(Color.black)
+                                        .padding(0).font(.system(size:24))
+                                    Text("test123")
+                                        .font(.system(size: 30))
+                                        .foregroundColor(Color.black)
+                                }.font(.system(size: 30))
+                                    .frame(minWidth: 0, maxWidth: UIScreen.screenWidth-10)
+                                    .background(colors[$0 % colors.count])
+                                    .cornerRadius(10)
+                            }
+                            
+                            
+                            
                         }
-                        
                         
                         
                     }
                     
-                    
-                    
-                    
                 }
-            }
-            
-                Color("redcolor").ignoresSafeArea()
-                
-                Color("bluecolor").edgesIgnoringSafeArea(.all)
-            
-                List(model.list) { item in Text(item.title) }
                 
             }
+            
+            Color("redcolor").ignoresSafeArea()
+            
+            Color("bluecolor").edgesIgnoringSafeArea(.all)
+            
+            List(model.list) { item in Text(item.title) }
             
         }
+        
+    }
+            
     
     init() {
         model.getData()

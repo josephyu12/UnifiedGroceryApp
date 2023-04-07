568939acd44413c2f02be1ebb18011d99be9a6d5
travtran <ttran2@wpi.edu>
2023-04-07 09:55:54 -0400

    4/7

--- UnifiedGroceryApp/Views/RecipeView.swift
+++ UnifiedGroceryApp/Views/RecipeView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RecipeView: View {
     
     @ObservedObject var model = ViewModel()
-      
+    
     private var gridLayout = [GridItem(.flexible())]
     
     private var colors: [Color] = [.yellow, .purple, .green]
@@ -20,62 +20,54 @@ struct RecipeView: View {
     
     var body: some View {
         
-        ZStack {
+        ZStack (alignment: .top) {
             
-            ZStack (alignment: .top) {
-                
-                Color("redcolor").ignoresSafeArea()
-                
-                Color("bluecolor").edgesIgnoringSafeArea(.top)
-                
-                ScrollView(showsIndicators: false) {
-                    VStack {
-                        Color("bluecolor").edgesIgnoringSafeArea(.all)
-                        Text("Recipes").font(.largeTitle).padding(.top, 0.1)
+            Color("redcolor").ignoresSafeArea()
+            
+            Color("bluecolor").edgesIgnoringSafeArea(.top)
+            
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    
+                    Color("bluecolor").edgesIgnoringSafeArea(.all)
+                    Text("Recipes").font(.largeTitle).padding(.top, 0.1)
+                    
+                    LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
                         
-                        
-                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                            
-                            ForEach((0...9), id: \.self) {
-                                Button(action: {print("this is the action run")} ) {
-                                    Image(systemName: "gear").foregroundColor(Color.black)
-                                        .padding(0).font(.system(size:24))
-                                    Text("test123")
-                                        .font(.system(size: 30))
-                                        .foregroundColor(Color.black)
-                                }.font(.system(size: 30))
-                                    .frame(minWidth: 0, maxWidth: UIScreen.screenWidth-10)
-                                    .background(colors[$0 % colors.count])
-                                    .cornerRadius(10)
-                            }
-                            
-                            
-                            
+                        List(model.list) { item in Text(item.title) }
+     
+                        ForEach((0...9), id: \.self) {
+                            Button(action: {print("this is the action run")} ) {
+                                Image(systemName: "gear").foregroundColor(Color.black)
+                                    .padding(0).font(.system(size:24))
+                                Text("test123")
+                                    .font(.system(size: 30))
+                                    .foregroundColor(Color.black)
+                            }.font(.system(size: 30))
+                                .frame(minWidth: 0, maxWidth: UIScreen.screenWidth-10)
+                                .background(colors[$0 % colors.count])
+                                .cornerRadius(10)
                         }
                         
                         
+                        
                     }
+                    
                     
                 }
                 
             }
             
-            Color("redcolor").ignoresSafeArea()
-            
-            Color("bluecolor").edgesIgnoringSafeArea(.all)
-            
-            List(model.list) { item in Text(item.title) }
-            
         }
         
     }
-            
+    
     
     init() {
         model.getData()
     }
-   
-    }
+    
+}
     
 
 struct RecipeView_Previews: PreviewProvider {
